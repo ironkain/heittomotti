6bf2f77dda14c34cf88b8a11c12093d62db592e6
Ismo Ronkainen <ismo.ronkainen@gmail.com>
2017-09-11 14:07:29 +0300

    email msg

--- demo/views.py
+++ demo/views.py
@@ -46,6 +46,10 @@ def tilaaminen(request):
                 # server.quit()
 
             import smtplib
+            from email.MIMEMultipart import MIMEMultipart
+            from email.MIMEText import MIMEText
+            from email.MIMEBase import MIMEBase
+            from email import encoders
             from email.mime.multipart import MIMEMultipart
             msg = MIMEMultipart()
             msg['From'] = 'myynti@heittomotti.fi'
